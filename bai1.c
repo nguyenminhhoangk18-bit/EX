29eb46eeb4bdd4beeef71654ffeeecfd2cedc051
HoangWype <nguyenminhhoangk18@siu.edu.vn>
2026-09-15 23:43:17 +0700

    first

---
+++ bai1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+void swap (int a,int b)
+{
+    int temp=a;
+    a=b;
+    b=temp;
+}
+int main()
+{   
+    int a,b;
+    printf("Hay nha gia tri a:");
+    scanf("%d",&a);
+    printf("Hay nha gia tri b:");
+    scanf("%d",&b);
+    swap(a,b);
+    printf("Gia tri a luc sau %d,Gia tri b luc sau %d",b,a);
+    return 0;
+}
